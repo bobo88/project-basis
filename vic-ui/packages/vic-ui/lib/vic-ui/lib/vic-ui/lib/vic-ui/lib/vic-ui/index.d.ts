b72161972f4f10fc435db829e3021dc13a432788
bobo88 <yuanboi88@163.com>
2022-08-12 09:38:29 +0800

    add

---
+++ vic-ui/packages/vic-ui/lib/vic-ui/lib/vic-ui/lib/vic-ui/lib/vic-ui/index.d.ts
@@ -0,0 +1,6 @@
+import installer from './defaults';
+export * from '@vic-ui/components';
+export * from '@vic-ui/constants';
+export * from './make-installer';
+export declare const install: (app: import("vue").App<any>) => void;
+export default installer;
